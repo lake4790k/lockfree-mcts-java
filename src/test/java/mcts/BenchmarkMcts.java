55f30adc0da3b91ea31ec1b181cc3e3f40e08fb5
lake4790k <lake4790k@users.noreply.github.com>
2016-06-12 00:48:41 +0200

    Benchmark

--- src/test/java/mcts/BenchmarkMcts.java
+++ src/test/java/mcts/BenchmarkMcts.java
@@ -5,18 +5,15 @@ import java.util.Arrays;
 public class BenchmarkMcts {
     private final int numCpu = Runtime.getRuntime().availableProcessors();
     private final int times = 200;
-    private final int maxIterations2 = 200;
+    private final int maxIterations2 = 500;
     private final int timePerActionSec = 999;
     private final int dim = 5;
     private final int needed = 4;
 
-    private int[] scores;
-    private SelfPlay<TicTacToe> play;
-
     private void run() {
         int t = 1;
         while (t <= numCpu) {
-            testScores(1, t * maxIterations2);
+            int[] scores = testScores(1, t * maxIterations2);
             int winPercent = (int) (100. * scores[1] / times);
             System.out.printf(
                 "1x%d vs 1x%d: %d%% %s\n",
@@ -26,10 +23,10 @@ public class BenchmarkMcts {
                 Arrays.toString(scores));
 
             if (t == 1) {
-                t = t * 2;
+                t *= 2;
                 continue;
             }
-            testScores(t, maxIterations2);
+            scores = testScores(t, maxIterations2);
             winPercent = (int) (100. * scores[1] / times);
             System.out.printf(
                 "%dx%d vs 1x%d: %d%% %s\n",
@@ -39,16 +36,16 @@ public class BenchmarkMcts {
                 winPercent,
                 Arrays.toString(scores));
 
-            t = t * 2;
+            t *= 2;
         }
     }
 
-    private void testScores(int threads1, int maxIterations1) {
-        scores = new int[3];
+    private int[] testScores(int threads1, int maxIterations1) {
+        int[] scores = new int[3];
         int threads2 = 1;
         for (int i = 0; i < times; i++) {
             TicTacToe startState = TicTacToe.start(dim, needed);
-            play = new SelfPlay<>(
+            SelfPlay<TicTacToe> play = new SelfPlay<>(
                 startState,
                 threads1,
                 threads2,
@@ -62,6 +59,7 @@ public class BenchmarkMcts {
             scores[winner]++;
             play.stop();
         }
+        return scores;
     }
 
     public static void main(String[] args) {
