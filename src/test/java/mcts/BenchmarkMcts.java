225c54ea8d970a563e4c6affd716ef123d46333c
lake4790k <lake4790k@users.noreply.github.com>
2016-06-14 18:33:43 +0200

    Gnuplot benchmark

--- src/test/java/mcts/BenchmarkMcts.java
+++ src/test/java/mcts/BenchmarkMcts.java
@@ -11,34 +11,53 @@ public class BenchmarkMcts {
     private final int timePerActionSec = 999000;
     private final int dim = 5;
     private final int needed = 3;
+    private long took;
 
     private void run() {
         int t = 1;
+
+        int[][] plot = new int[2][numCpu];
+
         while (t <= numCpu) {
             int[] scores = testScores(1, t * maxIterations2);
             int winPercent = (int) (100. * scores[1] / times);
             System.out.printf(
-                "1x%d vs 1x%d: %d%% %s\n",
+                "1x%d vs 1x%d: %d%% %s in %d s\n",
                 t * maxIterations2,
                 maxIterations2,
                 winPercent,
-                Arrays.toString(scores));
+                Arrays.toString(scores),
+                took);
+
+            plot[0][t - 1] = winPercent;
 
             if (t == 1) {
-                t *= 2;
+                plot[1][t - 1] = winPercent;
+                t++;
                 continue;
             }
+
             scores = testScores(t, maxIterations2);
             winPercent = (int) (100. * scores[1] / times);
             System.out.printf(
-                "%dx%d vs 1x%d: %d%% %s\n",
+                "%dx%d vs 1x%d: %d%% %s in %d s\n",
                 t,
                 maxIterations2,
                 maxIterations2,
                 winPercent,
-                Arrays.toString(scores));
+                Arrays.toString(scores),
+                took);
 
-            t *= 2;
+            plot[1][t - 1] = winPercent;
+            t++;
+        }
+
+        for (int i = 0; i < numCpu; i++) {
+            System.out.printf("%d\t%d\n", i + 1, plot[0][i]);
+        }
+        System.out.println("\n");
+        for (int i = 0; i < numCpu; i++) {
+            System.out.printf("%d\t%d\n", i + 1, plot[1][i]);
         }
     }
 
@@ -51,6 +70,7 @@ public class BenchmarkMcts {
             ? Executors.newFixedThreadPool(threads2)
             : null;
 
+        long start = System.currentTimeMillis();
         int[] scores = new int[3];
         for (int i = 0; i < times; i++) {
             TicTacToe startState = TicTacToe.start(dim, needed);
@@ -69,7 +89,7 @@ public class BenchmarkMcts {
 
             scores[winner]++;
         }
-
+        took = (System.currentTimeMillis() - start) / 1000;
         if (executor1 != null)
             executor1.shutdown();
         if (executor2 != null)
